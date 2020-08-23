4769c484722343c09031bee20ae3098521dd9b37
Mikayel Ghazaryan <mk@mk.am>
2020-08-24 00:24:30 +0200

    adding get_quotes

--- digikey/v3/api.py
+++ digikey/v3/api.py
@@ -3,12 +3,72 @@ import logging
 from distutils.util import strtobool
 import digikey.oauth.oauth2
 import digikey.v3.productinformation as dpi
+import digikey.v3.quoting as dq
+
 from digikey.exceptions import DigikeyError
 from digikey.v3.productinformation import (KeywordSearchRequest, KeywordSearchResponse, ProductDetails, DigiReelPricing,
                                            ManufacturerProductDetailsRequest)
 from digikey.v3.productinformation.rest import ApiException
+from digikey.v3.quoting import (QuoteResponse)
 
 logger = logging.getLogger(__name__)
+
+
+class QuoteApiWrapper(object):
+    def __init__(self, wrapped_function):
+        self.sandbox = False
+
+        # Configure API key authorization: apiKeySecurity
+        configuration = dq.Configuration()
+        configuration.api_key['X-DIGIKEY-Client-Id'] = os.getenv('DIGIKEY_CLIENT_ID')
+
+        # Return quitly if no clientid has been set to prevent errors when importing the module
+        if os.getenv('DIGIKEY_CLIENT_ID') is None or os.getenv('DIGIKEY_CLIENT_SECRET') is None:
+            raise DigikeyError('Please provide a valid DIGIKEY_CLIENT_ID and DIGIKEY_CLIENT_SECRET in your env setup')
+
+        # Use normal API by default, if DIGIKEY_CLIENT_SANDBOX is True use sandbox API
+        configuration.host = 'https://api.digikey.com/Search/v3'
+        try:
+            if bool(strtobool(os.getenv('DIGIKEY_CLIENT_SANDBOX'))):
+                configuration.host = 'https://sandbox-api.digikey.com/Search/v3/'
+                self.sandbox = True
+        except (ValueError, AttributeError):
+            pass
+
+        # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+        # configuration.api_key_prefix['X-DIGIKEY-Client-Id'] = 'Bearer'
+
+        # Configure OAuth2 access token for authorization: oauth2AccessCodeSecurity
+        self._digikeyApiToken = digikey.oauth.oauth2.TokenHandler(version=3, sandbox=self.sandbox).get_access_token()
+        configuration.access_token = self._digikeyApiToken.access_token
+
+        # create an instance of the API class
+        self._api_instance = dq.QuotingApi(dpi.ApiClient(configuration))
+
+        # Populate reused ids
+        self.authorization = self._digikeyApiToken.get_authorization()
+        self.x_digikey_client_id = os.getenv('DIGIKEY_CLIENT_ID')
+
+        self.wrapped_function = wrapped_function
+
+    @staticmethod
+    def _print_remaining_requests(header):
+        try:
+            rate_limit = header['X-RateLimit-Limit']
+            rate_limit_rem = header['X-RateLimit-Remaining']
+            logger.debug('Requests remaining: [{}/{}]'.format(rate_limit_rem, rate_limit))
+        except KeyError:
+            pass
+
+    def call_api_function(self, *args, **kwargs):
+        try:
+            func = getattr(self._api_instance, self.wrapped_function)
+            logger.debug(f'CALL wrapped -> {func.__qualname__}')
+            api_response = func(*args, self.authorization, self.x_digikey_client_id, **kwargs)
+            self._print_remaining_requests(api_response[2])
+            return api_response[0]
+        except ApiException as e:
+            logger.error(f'Exception when calling {self.wrapped_function}: {e}')
 
 
 class ProductApiWrapper(object):
@@ -111,3 +171,11 @@ def manufacturer_product_details(*args, **kwargs) -> KeywordSearchResponse:
         return client.call_api_function(*args, **kwargs)
     else:
         raise DigikeyError('Please provide a valid ManufacturerProductDetailsRequest argument')
+
+
+def get_quotes(*args, **kwargs) -> QuoteResponse:
+    client = QuoteApiWrapper('get_quotes')
+
+    if len(args):
+        logger.info(f'Retrieves a list of quotes owned by a customer or associated customer. Quote details will not be populated.  Seperate GET requests must be made for details.')
+        return client.call_api_function(*args, **kwargs)
